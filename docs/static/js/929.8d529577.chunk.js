0c9f7254b7c33ff86b2a9932aa6bca33ca5abe04
egorrya <fayncore@gmail.com>
2023-06-11 19:35:42 +0300

    build

--- docs/static/js/929.8d529577.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkpersonal_portfolio=self.webpackChunkpersonal_portfolio||[]).push([[929],{9929:function(e,o,p){e.exports=p.p+"static/media/CV Egor Riabysh.8c7a0572eae2d5070159.pdf"}}]);
