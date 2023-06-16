b05a12b77f80e9979bebd317d43b6bdf251c57bf
egorrya <fayncore@gmail.com>
2023-06-17 02:21:10 +0300

    new cv

---
+++ docs/static/js/929.507a7ae9.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpersonal_portfolio=self.webpackChunkpersonal_portfolio||[]).push([[929],{9929:function(o,p,s){o.exports=s.p+"static/media/CV Egor Riabysh.6bc2d8c032dacb51489c.pdf"}}]);
--- docs/static/js/929.e3fe36af.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkpersonal_portfolio=self.webpackChunkpersonal_portfolio||[]).push([[929],{9929:function(e,o,p){e.exports=p.p+"static/media/CV Egor Riabysh.e153e4d185cbb32f57d0.pdf"}}]);
